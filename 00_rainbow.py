1b2f23a1938e1bccfe72e49b28759f531969aed2
Kavish Kartha <38027212+kavishkartha1o1@users.noreply.github.com>
2019-10-19 13:38:31 -0700

    Add files via upload

---
+++ 00_rainbow.py
@@ -0,0 +1,17 @@
+def write_colorful_text():
+    print('\x1b[1;31;40m' + 'R' + '\x1b[0,m')
+    
+    print('\x1b[1;33;40m' + 'A' + '\x1b[0,m')
+    
+    print('\x1b[1;36;40m' + 'I' + '\x1b[0,m')
+    
+    print('\x1b[1;32;40m' + 'N' + '\x1b[0,m')
+    
+    print('\x1b[1;34;40m' + 'B' + '\x1b[0,m')
+    
+    print('\x1b[1;35;40m' + 'O' + '\x1b[0,m')
+    
+    print('\x1b[1;37;40m' + 'W' + '\x1b[0,m')
+
+write_colorful_text()
+
